3fa739ecf81fece29639f23bc7bb1a22177c85fa
lukevangorder <lukevangorder@gmail.com>
2021-01-13 15:36:50 -0500

    commit tree 49e0db87f2b496d52502c93a10af2fa5ff511dcb

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
+def my_collect(array)
+    sendBack = []
+    i=0
+    while i<array.length
+        yield(array[i])
+        sendBack << yield(array[i])
+        i+=1
+    end
+    sendBack
+end
+        
 
